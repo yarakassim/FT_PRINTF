54e0a20afcab929c1919f4934c7d9d6c8ea6b71c
ykassim <yskp2901@gmail.com>
2021-09-22 17:30:38 +0200

    opti

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ykassim- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 13:36:27 by ykassim-          #+#    #+#             */
-/*   Updated: 2021/09/18 17:01:16 by ykassim-         ###   ########.fr       */
+/*   Updated: 2021/09/22 17:30:21 by ykassim-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,12 @@
 
 int	ft_printf(const char *str, ...)
 {
-	t_tools	*play;
-	int		ret;
+	/*struct doesn't need to be declared as a pointer*/
+	t_tools	play;
 
-	play = (t_tools *)malloc(sizeof(t_tools));
-	if (!play)
-		return (-1);
-	play->index = 0;
-	va_start(play->args, str);
-	parse_away(str, play);
-	va_end(play->args);
-	ret = play->index;
-	free(play);
-	return (ret);
+	play.index = 0;
+	va_start(play.args, str);
+	parse_away(str, &play);
+	va_end(play.args);
+	return (play.index);
 }
